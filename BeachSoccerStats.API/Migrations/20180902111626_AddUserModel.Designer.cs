14618cbb1635c5e34836d2cec682101332ef02fb
Mateusz Szymanski <sejman92@gmail.com>
2018-09-02 13:18:06 +0200

    User mode and migration

--- BeachSoccerStats.API/Migrations/20180902111626_AddUserModel.Designer.cs
+++ BeachSoccerStats.API/Migrations/20180902111626_AddUserModel.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BeachSoccerStats.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BeachSoccerStats.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180902111626_AddUserModel")]
+    partial class AddUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
