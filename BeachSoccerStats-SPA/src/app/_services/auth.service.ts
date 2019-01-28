5539d5f8a554b559f77ef25314b27d5dc9c78ada
Mateusz Szymanski <sejman92@gmail.com>
2019-01-28 22:33:11 +0100

    Implement usage of alertify.  Display logged in username on navbar

--- BeachSoccerStats-SPA/src/app/_services/auth.service.ts
+++ BeachSoccerStats-SPA/src/app/_services/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
-constructor(private http: HttpClient) { }
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
+constructor(public http: HttpClient) { }
 
   login(model: any ) {
     return this.http.post(this.baseUrl + 'login', model)
@@ -16,11 +19,17 @@ constructor(private http: HttpClient) { }
               const user = response;
               if (user) {
                 localStorage.setItem('token', user.token);
+                this.decodedToken = this.jwtHelper.decodeToken(user.token);
+                console.log(this.decodedToken);
               }
             })
           );
   }
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
