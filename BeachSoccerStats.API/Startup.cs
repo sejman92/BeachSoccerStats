a5c2dc54f15bb50b81dae957f39a408c6448bd82
Mateusz Szymanski <sejman92@gmail.com>
2019-02-03 22:29:10 +0100

    new repository for dating

--- BeachSoccerStats.API/Startup.cs
+++ BeachSoccerStats.API/Startup.cs
@@ -39,6 +39,7 @@ namespace BeachSoccerStats.API
             services.AddCors();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
                 .AddJwtBearer(options => {
